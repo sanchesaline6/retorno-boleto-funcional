315fa7a15258fb7ea9e03b1b60f841dae6655ed8
sanchesaline6 <sanches.aline6@gmail.com>
2022-04-17 08:43:35 -0300

    Implementa algoritmo de leitura boleto do Banco do Brasil

--- src/LeituraRetornoBancoBrasil.java
+++ src/LeituraRetornoBancoBrasil.java
@@ -1,8 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LeituraRetornoBancoBrasil implements LeituraRetorno{
     @Override
     public List<Boleto> lerArquivo(String nomeArquivo) {
-        return null;
+        try{
+            BufferedReader reader = Files.newBufferedReader(Paths.get(nomeArquivo));
+            String line;
+            List<Boleto> boletos = new ArrayList<>();
+
+            while((line = reader.readLine()) != null){
+                String[] vetor = line.split(";");
+                Boleto boleto = new Boleto();
+                boleto.setId(Integer.parseInt(vetor[0]));
+                boleto.setCodBanco(vetor[1]);
+
+                boleto.setDataVencimento(LocalDate.parse(vetor[2], ProcessarBoletos.FORMATO_DATA));
+                boleto.setDataPagamento(LocalDate.parse(vetor[3], ProcessarBoletos.FORMATO_DATA_HORA).atTime(0,0,0));
+
+                boleto.setCpfCliente(vetor[4]);
+                boleto.setValor(Double.parseDouble(vetor[5]));
+                boleto.setMulta(Double.parseDouble(vetor[6]));
+                boleto.setJuros(Double.parseDouble(vetor[7]));
+
+                boletos.add(boleto);
+            }
+            return boletos;
+        }
+        catch (IOException e){
+            throw new UncheckedIOException(e);
+        }
     }
 }
